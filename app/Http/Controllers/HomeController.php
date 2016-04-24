7e8ee5d1e4d7a4073ce3a7dac5c961ee264c9841
theroppex <theroppex@gmail.com>
2016-04-24 15:56:47 +0200

    Added option to display cards associated with certain user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        
+        $notes = $user->notes;
+        
+        
+        return view('home', compact('user', 'notes'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
-        
-        <div class="container-fluid col-md-4 ">
-            <div class="card default-color white-text hoverable">
-                <div class="card-content">
-                    
-                    <span class="card-title">Card Title</span>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula.</p>
+        @if(count($notes)===0)
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 text-center">
+                        
+                            <h1 >Hey, {{$user->name}}.</h1>
+                            <h5 class="muted">Start noting</h5>
+                    </div>
                 </div>
-                <div class="card-action">
-                    <a class=""><i class="material-icons white-text">delete</i></a>
+            </div>
+        @else
+            @foreach ($notes as $note)
+                <div class="container-fluid col-md-4 ">
+                    <div class="card default-color white-text hoverable">
+                        <div class="card-content">
+                            
+                            <span class="card-title">{{$note->title}}</span>
+                            <hr>
+                            <p>{{$note->body}}</p>
+                        </div>
+                        <div class="card-action">
+                            <a class=""><i class="material-icons white-text">delete</i></a>
+                        </div>
+                    </div>   
                 </div>
-            </div>   
-        </div>
-        <div class="container-fluid col-md-4 ">
-            <div class="card default-color white-text hoverable">
-                <div class="card-content">
-                    
-                    <span class="card-title">Card Title</span>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula.</p>
-                </div>
-                <div class="card-action">
-                    <a class=""><i class="material-icons white-text">delete</i></a>
-                </div>
-            </div>   
-        </div>
-        <div class="container-fluid col-md-4 ">
-            <div class="card default-color white-text hoverable">
-                <div class="card-content">
-                    
-                    <span class="card-title">Card Title</span>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula.</p>
-                </div>
-                <div class="card-action">
-                    <a class=""><i class="material-icons white-text">delete</i></a>
-                </div>
-            </div>   
-        </div>
-        
-        
+            @endforeach
+        @endif
     </div>
-    
 </div>
 
 <a class="btn-floating btn-small waves-effect waves-light red floating-button"><i class="material-icons">add</i></a>
